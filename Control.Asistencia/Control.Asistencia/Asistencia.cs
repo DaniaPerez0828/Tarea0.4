5c94f8461fc27df453330abd5f848ecb00041f61
Dannia Perez <danniaperezri2808@gmail.com>
2024-10-07 21:38:18 -0600

    se creo un metodo para calcular el porcentaje se asistencias

--- Control.Asistencia/Control.Asistencia/Asistencia.cs
+++ Control.Asistencia/Control.Asistencia/Asistencia.cs
@@ -9,5 +9,11 @@ namespace Control.Asistencia
     public static class Asistencia
     {
         private const double MINIMO_ASISTENCIA = 75;
+        public static double CalcularPorcentaje(int asistidas, int totales)
+        {
+            if (totales == 0)
+                return 0;
+            return (double)asistidas / totales * 100;
+        }
     }
 }
