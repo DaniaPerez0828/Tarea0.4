26d5b697a0d1daf72ab21576e7084ad94f6a5c42
Dannia Perez <danniaperezri2808@gmail.com>
2024-10-07 21:53:12 -0600

    se crea un buvle para regsitrar datos y luego se llama al metodo registraEstudiante para almacenar la info

--- Control.Asistencia/Control.Asistencia/Program.cs
+++ Control.Asistencia/Control.Asistencia/Program.cs
@@ -19,6 +19,28 @@ namespace Control.Asistencia
                 Console.Write("Por favor, ingresa un número válido de estudiantes: ");
             }
 
+            for (int i = 0; i < numeroEstudiantes; i++)
+            {
+                Console.Write($"Ingresa el nombre del estudiante #{i + 1}: ");
+                string nombre = Console.ReadLine();
+
+                Console.Write($"Ingresa el número total de sesiones de {nombre}: ");
+                int sesionesTotales;
+                while (!int.TryParse(Console.ReadLine(), out sesionesTotales) || sesionesTotales <= 0)
+                {
+                    Console.Write("Por favor, ingresa un número válido de sesiones: ");
+                }
+
+                Console.Write($"¿Cuántas sesiones asistió {nombre}? ");
+                int sesionesAsistidas;
+                while (!int.TryParse(Console.ReadLine(), out sesionesAsistidas) || sesionesAsistidas < 0 || sesionesAsistidas > sesionesTotales)
+                {
+                    Console.Write("Por favor, ingresa un número válido de sesiones asistidas: ");
+                }
+
+                universidad.RegistrarEstudiante(nombre, sesionesTotales, sesionesAsistidas);
+            }
+
         }
 
     }
