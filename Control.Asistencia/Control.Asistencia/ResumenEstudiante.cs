b07166b43340b7acbfce828ef5e82aca42f595e2
Dannia Perez <danniaperezri2808@gmail.com>
2024-10-07 21:20:10 -0600

    se creo una clase para mostrar la info del estudiante

---
+++ Control.Asistencia/Control.Asistencia/ResumenEstudiante.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control.Asistencia
+{
+    public static class ResumenEstudiante
+    {
+        public static string ObtenerInfo(Estudiante estudiante)
+        {
+            return estudiante.ObtenerInfo();
+        }
+    }
+}
